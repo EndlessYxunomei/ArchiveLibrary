16000304122c70834cc461ec9186673259ad820a
Глеб Пестрецов <pestretsov.gleb@live.ru>
2025-07-10 12:09:59 +0300

    Сделан конвертер для DocumentListDto вместо MulitBinding - убралась ошибка биндингов в выпадающем списке

---
+++ ArchiveLibrary/Converters/DocumentDtoDisplayConverter.cs
@@ -0,0 +1,20 @@
+﻿using ArchiveModels.DTO;
+using System.Globalization;
+
+namespace ArchiveLibrary.Converters;
+
+public class DocumentDtoDisplayConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DocumentListDto documentListDto)
+        {
+            return $"{documentListDto.Name} от {documentListDto.Date:d}";
+        }
+        else return string.Empty ;
+
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => null;
+
+}
